5906ab0abc30d5b404e2f6e09fe0851b6a71104f
Nissyaniss <kirito042004@gmail.com>
2023-10-26 18:05:58 +0200

    allumettes cleanups again

--- SAÉ/Mini-jeux/allumettes.py
+++ SAÉ/Mini-jeux/allumettes.py
@@ -1,12 +1,10 @@
-from os import system
 from colorama import Fore, Back
-from termUtils import printAt
 import sys
 from time import sleep
 from os import get_terminal_size
 
 from players import addPoint
-from termUtils import displayEmptySquare, printAt, centerTextAtLine, centerText, centerTextAtColumn, setCursorPositionCenterAtColumn
+from termUtils import displayEmptySquare, centerTextAtLine, centerText
 
 def DisplayMenu(currentSelectedNb : int, player : str, matchs : int) -> str:
 	maxHeight : int
@@ -31,46 +29,40 @@ def start(player1 : str, player2 : str) -> None:
 	matchs : int
 	currChar : str
 	currentSelectedNb : int
-	currentPlayer : int
+	currentPlayer : str
 	maxHeight : int
 
 	maxHeight = get_terminal_size().lines - 3
-	currentPlayer = 1
+	currentPlayer = player1
 	matchs = 20
 	currChar = ""
 	currentSelectedNb = 1
 
 	displayEmptySquare()
-	centerTextAtLine(maxHeight // 2 + 2 + currentSelectedNb, DisplayMenu(currentSelectedNb, player1, matchs))
 	while True:
 		while True:
+			if currentPlayer == player1:
+				centerTextAtLine(maxHeight // 2 + 2 + currentSelectedNb, DisplayMenu(currentSelectedNb, player1, matchs))
+			else:
+				centerTextAtLine(maxHeight // 2 + 2 + currentSelectedNb, DisplayMenu(currentSelectedNb, player2, matchs))
 			currChar = sys.stdin.read(1)
 			if currChar == '\x1b':
 				currChar = sys.stdin.read(1)
 				currChar = sys.stdin.read(1)
 				if currChar == 'A' and currentSelectedNb != 1:
 					currentSelectedNb -= 1
-					if currentPlayer == 1:
-						centerTextAtLine(maxHeight // 2 + 2 + currentSelectedNb, DisplayMenu(currentSelectedNb, player1, matchs))
-					else:
-						centerTextAtLine(maxHeight // 2 + 2 + currentSelectedNb, DisplayMenu(currentSelectedNb, player2, matchs))
 				if currChar == 'B' and currentSelectedNb != 3:
 					currentSelectedNb += 1
-					if currentPlayer == 1:
-						centerTextAtLine(maxHeight // 2 + 2 + currentSelectedNb,DisplayMenu(currentSelectedNb, player1, matchs))
-					else:
-						centerTextAtLine(maxHeight // 2 + 2 + currentSelectedNb,DisplayMenu(currentSelectedNb, player2, matchs))
 			elif currChar == 'q' or currChar == 'Q':
 				return
 			elif currChar == '\n':
 				matchs = matchs - currentSelectedNb
 				if matchs == 0:
-					if currentPlayer == 1:
-						displayEmptySquare()
+					displayEmptySquare()
+					if currentPlayer == player1:
 						centerText(f"{player2} a gagné")
 						addPoint(player2, 2)
 					else:
-						displayEmptySquare()
 						centerText(f"{player1} a gagné")
 						addPoint(player1, 2)
 					sleep(1)
@@ -78,9 +70,7 @@ def start(player1 : str, player2 : str) -> None:
 				elif matchs < 0:
 					matchs = matchs + currentSelectedNb
 				else:
-					if currentPlayer == 1:
-						currentPlayer = 2
-						centerTextAtLine(maxHeight // 2 + 2 + currentSelectedNb, DisplayMenu(currentSelectedNb, player2, matchs))
+					if currentPlayer == player1:
+						currentPlayer = player2
 					else:
-						currentPlayer = 1
-						centerTextAtLine(maxHeight // 2 + 2 + currentSelectedNb, DisplayMenu(currentSelectedNb, player1, matchs))
+						currentPlayer = player1
