9799d37c80bb91822ba94e2324c384be93e4c296
Nissya <kirito042004@gmail.com>
2023-11-13 15:11:12 +0100

    some cleaning and rules commenté

--- SAÉ/Mini-jeux/rules.py
+++ SAÉ/Mini-jeux/rules.py
@@ -4,6 +4,10 @@ from colorama import Fore, Back
 from termUtils import printAt, displayEmptySquare, centerTextAtLine, getKey
 
 def displayMenu() -> None:
+	"""
+	Affiche le menu
+	"""
+
 	displayEmptySquare()
 
 	centerTextAtLine(6, "┌─────────────────────────┐")
@@ -12,12 +16,21 @@ def displayMenu() -> None:
 	centerTextAtLine(9, "└─────────────────────────┘")
 
 def displayGameSelected(currentSelectedGame : int) -> str:
+	"""
+	Affiche le jeu sélectionner
+
+	Entrée : currentSelectedGame : int
+	currentSelectedGame symbolise le jeu sélectionner
+
+	Sortie : str
+	str symbolise le jeu sélectionner formaté
+	"""
 	gameStr: str
 	maxWidth = get_terminal_size().columns - 1
 
 	if currentSelectedGame == 1:
-		gameStr = "Devinette"
-		printAt(12, (maxWidth - 13) // 2, "  Allumette")
+		gameStr = "Devinette" # Met gameStr à la valeur du jeu sélectionner formaté
+		printAt(12, (maxWidth - 13) // 2, "  Allumette") # Affiche les autres jeux
 		printAt(13, (maxWidth - 13) // 2, "  Morpion")
 		printAt(14, (maxWidth - 13) // 2, "  Puissance 4")
 	elif currentSelectedGame == 2:
@@ -41,45 +54,60 @@ def displayGameSelected(currentSelectedGame : int) -> str:
 	print()
 	return ">" + Back.WHITE + Fore.BLACK + gameStr + Back.RESET + Fore.RESET + " "
 
-def printRule(gameStr : str, ruleStr : str, currentSelectedGame : int, player1 : str, player2 : str) -> None:
+def printRule(gameStr : str, ruleStr : str) -> None:
+	"""
+	Affiche la règle du jeu sélectionner
+
+	Entrée : gameStr : str
+	gameStr symbolise le jeu sélectionner
+
+	Entrée : ruleStr : str
+	ruleStr symbolise la règle du jeu sélectionner
+	"""
 	currChar : str
 	
 	currChar = ''
 
 	displayEmptySquare()
-	centerTextAtLine(2, "┌" + len(gameStr) * "─" + "┐")
+	centerTextAtLine(2, "┌" + len(gameStr) * "─" + "┐") # Affiche le menu des règles du jeu sélectionner
 	centerTextAtLine(3, "│" + f"{gameStr}" +       "│")
 	centerTextAtLine(4, "└" + len(gameStr) * "─" + "┘")
-	centerTextAtLine(8, ruleStr)
+	centerTextAtLine(8, ruleStr) # Affiche la règle du jeu sélectionner
 	while True:
 		currChar = getKey()
 		if currChar == "TAB":
 			return
 
-def start(currentSelectedGame : int, player1 : str, player2 : str) -> None:
+def start(currentSelectedGame : int) -> None:
+	"""
+	Lance le menu des règles
+
+	Entrée : currentSelectedGame : int
+	currentSelectedGame symbolise le jeu sélectionner
+	"""
 	maxWidth : int
 
-	maxWidth = get_terminal_size().columns - 2
+	maxWidth = get_terminal_size().columns - 3 # Récupère la taille du terminal
 
 	while True:
 		while True:
 			displayMenu()
-			printAt(10 + currentSelectedGame, (maxWidth - 12) // 2, displayGameSelected(currentSelectedGame))
+			printAt(10 + currentSelectedGame, (maxWidth - 12) // 2, displayGameSelected(currentSelectedGame)) # Affiche le jeu sélectionner
 			currChar = getKey()
-			if currChar == "UP" and currentSelectedGame != 1:
+			if currChar == "UP" and currentSelectedGame != 1: # Change le jeu sélectionner
 				currentSelectedGame -= 1
 			if currChar == "DOWN" and currentSelectedGame != 4:
 				currentSelectedGame += 1
-			elif currChar == "TAB":
+			elif currChar == "TAB": # Quitte au menu
 				return
 			elif currChar == "ENTER":
 				system("clear")
 				break
 		if currentSelectedGame == 1:
-			printRule("DEVINETTE", "Le joueur 1 rentre un chiffre et le joueur 2 doit le deviner en moins de temps possible !", 1, player1, player2)
+			printRule("DEVINETTE", "Le joueur 1 rentre un chiffre et le joueur 2 doit le deviner en moins de temps possible !")
 		elif currentSelectedGame == 2:
-			printRule("ALLUMETTES", "Chaque joueur retire 1 - 3 allumettes jusqu'a ce qui en ai plus ! Le perdant est celui qui prend la dernière.", 2, player1, player2)
+			printRule("ALLUMETTES", "Chaque joueur retire 1 - 3 allumettes jusqu'a ce qui en ai plus ! Le perdant est celui qui prend la dernière.")
 		elif currentSelectedGame == 3:
-			printRule("MORPION", "Chacun son tour les joueurs place un pion le premier à en aligner 3 a gagné !", 3, player1, player2)
+			printRule("MORPION", "Chacun son tour les joueurs place un pion le premier à en aligner 3 a gagné !")
 		elif currentSelectedGame == 4:
-			printRule("PUISSANCE 4", "Chacun son tour les joueurs place un pion le premier à en aligner 4 a gagné !", 4, player1, player2)
+			printRule("PUISSANCE 4", "Chacun son tour les joueurs place un pion le premier à en aligner 4 a gagné !")
