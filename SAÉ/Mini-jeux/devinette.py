39d014ea6cb37f81f56fe4806f20f86fcf8edf04
Nissya <kirito042004@gmail.com>
2023-11-13 15:55:44 +0100

    devinette commenté

--- SAÉ/Mini-jeux/devinette.py
+++ SAÉ/Mini-jeux/devinette.py
@@ -6,12 +6,24 @@ from players import addPoint
 from termUtils import setCursorPosition, displayEmptySquare, printAt, centerTextAtLine, centerText, getKey
 
 def displayMenuMaster() -> None:
+	"""
+	Affiche le menu du maître du jeu
+	"""
 	displayEmptySquare()
 	centerTextAtLine(6, "┌──────────────────────────────┐")
 	centerTextAtLine(7, "│    Comment est sa réponse    │")
 	centerTextAtLine(8, "└──────────────────────────────┘")
 
 def displaySelectedOption(currentSelectedOption : int) -> str:
+	"""
+	Affiche l'option sélectionnée
+
+	Entrée : currentSelectedOption : int
+	currentSelectedOption symbolise l'option sélectionnée
+
+	Sortie : str
+	Symbolise l'option sélectionnée formaté
+	"""
 	optionStr: str
 
 	if currentSelectedOption == 1:
@@ -32,18 +44,36 @@ def displaySelectedOption(currentSelectedOption : int) -> str:
 	return ">" + Back.WHITE + Fore.BLACK + optionStr + Back.RESET + Fore.RESET + "  "
 
 def displayMenuPlayer() -> None:
+	"""
+	Affiche le menu du joueur
+	"""
 	displayEmptySquare()
 	centerTextAtLine(6, "┌─────────────────────────────────┐")
 	centerTextAtLine(7, "│   Qui fera deviner à l'autre    │")
 	centerTextAtLine(8, "└─────────────────────────────────┘")
 
 def DisplaySelectedPlayer(currentSelectedPlayer : int, player1 : str, player2 : str) -> str:
+	"""
+	Affiche le joueur sélectionné
+
+	Entrée : currentSelectedPlayer : int
+	currentSelectedPlayer symbolise le joueur sélectionné
+
+	Entrée : player1 : str
+	player1 symbolise le nom du joueur 1
+
+	Entrée : player2 : str
+	player2 symbolise le nom du joueur 2
+	
+	Sortie : str
+	Symbolise le joueur sélectionné formaté
+	"""
 	playerStr: str
 	maxWidth : int
 
-	maxWidth = get_terminal_size().columns - 3
+	maxWidth = get_terminal_size().columns - 3 # Récupère la taille du terminal
 	
-	if currentSelectedPlayer == 1:
+	if currentSelectedPlayer == 1: # Affiche le joueur sélectionné
 		playerStr = player1
 		printAt(12, maxWidth // 2 - len(player1) + 3, "  " + player2)
 	elif currentSelectedPlayer == 2:
@@ -55,6 +85,15 @@ def DisplaySelectedPlayer(currentSelectedPlayer : int, player1 : str, player2 : 
 	return str(">" + Back.WHITE + Fore.BLACK + playerStr + Back.RESET + Fore.RESET + "  ")
 
 def start(player1 : str, player2 : str) -> None:
+	"""
+	Démarre le jeu
+
+	Entrée : player1 : str
+	player1 symbolise le nom du joueur 1
+
+	Entrée : player2 : str
+	player2 symbolise le nom du joueur 2
+	"""
 	solution : str
 	guess : str
 	currChar : str
@@ -67,7 +106,7 @@ def start(player1 : str, player2 : str) -> None:
 	maxTries : int
 	tries : int
 
-	maxWidth = get_terminal_size().columns - 3
+	maxWidth = get_terminal_size().columns - 3 # Récupère la taille du terminal
 	maxHeight = get_terminal_size().lines - 3
 	currentSelectedOption = 1
 	currentSelectedPlayer = 1
@@ -85,7 +124,7 @@ def start(player1 : str, player2 : str) -> None:
 	while True:
 		printAt(10 + currentSelectedPlayer, maxWidth // 2 - len(player1) + 3, DisplaySelectedPlayer(currentSelectedPlayer, player1, player2))
 		currChar = getKey()
-		if currChar == "UP" and currentSelectedPlayer != 1:
+		if currChar == "UP" and currentSelectedPlayer != 1: # Déplace le curseur
 			currentSelectedPlayer -= 1
 		elif currChar == "DOWN" and currentSelectedPlayer != 2:
 			currentSelectedPlayer += 1
@@ -101,7 +140,7 @@ def start(player1 : str, player2 : str) -> None:
 		guesser = player1
 	print("\x1b[?25h", end='')
 	displayEmptySquare()
-	centerText("Nombre : ")
+	centerText("Nombre : ") # Demande le nombre à faire deviner
 	while True:
 		currChar = getKey()
 		if currChar.isdigit() and len(solution) < 3:
@@ -115,7 +154,7 @@ def start(player1 : str, player2 : str) -> None:
 				solution = solution[:-1]
 	while True:
 		displayEmptySquare()
-		centerText("Devinez : " + len(solution) * " ")
+		centerText("Devinez : " + len(solution) * " ") # Demande le nombre à deviner
 		while True:
 			setCursorPosition(maxHeight // 2, (maxWidth // 2 + 4) + len(guess))
 			currChar = getKey()
@@ -135,7 +174,7 @@ def start(player1 : str, player2 : str) -> None:
 		while True:
 			centerTextAtLine(10 + currentSelectedOption, displaySelectedOption(currentSelectedOption))
 			currChar = getKey()
-			if currChar == "UP" and currentSelectedOption != 1:
+			if currChar == "UP" and currentSelectedOption != 1: # Déplace le curseur
 				currentSelectedOption -= 1
 			elif currChar == "DOWN" and currentSelectedOption != 3:
 				currentSelectedOption += 1
@@ -145,28 +184,28 @@ def start(player1 : str, player2 : str) -> None:
 				print("\x1b[?25h", end='', flush=True)
 				break
 		tries = tries + 1
-		if currentSelectedOption == 1 and int(guess) < int(solution):
+		if currentSelectedOption == 1 and int(guess) < int(solution): # Vérifie si la réponse est bonne
 			strikes += 1
 		elif currentSelectedOption == 2 and int(guess) > int(solution):
 			strikes =+ 1
-		elif (currentSelectedOption == 1 or currentSelectedOption == 2) and guess == solution:
+		elif (currentSelectedOption == 1 or currentSelectedOption == 2) and guess == solution: # Vérifie si le master a pas triché
 			strikes = 3
 		guess = ""
-		if strikes == 3:
+		if strikes == 3: # Vérifie si le master a pas triché
 			print("\x1b[?25l", end='', flush=True)
 			displayEmptySquare()
-			centerText(f"{guesser} a gagné car {master} a triché")
+			centerText(f"{guesser} a gagné car {master} a triché") 
 			addPoint(guesser, 1)
 			sleep(1)
 			return
-		elif tries == maxTries:
+		elif tries == maxTries: # Vérifie si le joueur a pas dépassé le nombre d'essais
 			print("\x1b[?25l", end='', flush=True)
 			displayEmptySquare()
 			centerText(f"{master} a gagné car {guesser} n'a pas trouvé en {maxTries} essais")
 			sleep(1)
 			addPoint(master, 1)
 			return
-		elif currentSelectedOption == 3:
+		elif currentSelectedOption == 3: # Vérifie si le joueur a trouvé
 			print("\x1b[?25l", end='', flush=True)
 			displayEmptySquare()
 			centerText(f"{guesser} a gagné car il a trouvé en {tries}/{maxTries} essais")
